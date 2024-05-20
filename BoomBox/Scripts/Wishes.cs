2d4baa40dedbe64265d10295441f71d6f62960d8
Christopher Klimas <caklimas@gmail.com>
2024-05-20 16:20:02 -0400

    Add building blocks
    Add building blocks for boom box item.
    
    Add wish command to spawn it in inventory

---
+++ BoomBox/Scripts/Wishes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XRL;
+using XRL.Wish;
+using XRL.World;
+using XRL.World.AI.GoalHandlers;
+
+namespace BoomBox.Scripts
+{
+    [HasWishCommand]
+    public class WishHandler
+    {
+        [WishCommand(Command = "getboombox")]
+        public static void GetBoomBox()
+        {
+            The.Player.Inventory.AddObjectToInventory(GameObject.Create("Cruxius_BoomBox_Boom Box"));
+        }
+    }
+}
