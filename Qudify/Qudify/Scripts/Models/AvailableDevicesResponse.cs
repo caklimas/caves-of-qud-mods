b75da6f8aa8ae0208e4fca7d0ff090eae7a10435
Christopher Klimas <caklimas@gmail.com>
2024-06-20 17:32:20 -0400

    Add complete quest step

--- Qudify/Qudify/Scripts/Models/AvailableDevicesResponse.cs
+++ Qudify/Qudify/Scripts/Models/AvailableDevicesResponse.cs
@@ -9,5 +9,10 @@ namespace Qudify.Scripts.Models
     internal class AvailableDevicesResponse
     {
         public SpotifyDevice[] devices { get; set; } = new SpotifyDevice[0];
+
+        public bool HasDevice(string deviceName)
+        {
+            return devices.Any(device => device.name == deviceName);
+        }
     }
 }
